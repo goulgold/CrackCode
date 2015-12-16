af107f5e31a5304841ded90c68347d533a427e6b
Ubuntu <ubuntu@ip-172-31-8-226.ec2.internal>
2015-12-16 06:44:05 +0000

    directed tree date structue

---
+++ directedtree.cc
@@ -0,0 +1,27 @@
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+#define PROB 60
+
+vector<pair<int, int> > DirectedTree(int size, int max_edge) {
+    vector<pair<int, int> > result;
+    srand(time(NULL));
+    int direction = 0;
+    pair<int, int> temp;
+    for (int i = 0; i < size-1; ++i) {
+        for (int j = i+1; j < size; ++j) {
+            if (rand() % 100 < PROB) {
+                direction = rand() % 2;
+                if (direction)
+                    temp = make_pair(i, j);
+                else
+                    temp = make_pair(j, i);
+                result.push_back(temp);
+            }
+        }
+    }
+    return result;
+}
